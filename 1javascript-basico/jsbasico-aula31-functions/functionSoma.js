4ad93ae3b3b2be6c892c9bab90a6cceb869f954a
jetta44 <kaikymourasousa46@gmail.com>
2024-10-24 00:03:12 -0300

    commit

---
+++ 1javascript-basico/jsbasico-aula31-functions/functionSoma.js
@@ -0,0 +1,19 @@
+// function soma(x, y) {
+//     const resultado = `O valor de soma de ${x} e ${y} é: ${x + y}`
+//     return resultado;
+// }
+
+// somando = soma(10, 15.2);
+// console.log(somando);
+
+// const raizQuadrada = function(n) {
+// 	return n ** 0.5
+// };
+
+// console.log(raizQuadrada(9));
+// console.log(raizQuadrada(16));
+// console.log(raizQuadrada(25));
+
+const raizQuadrada = n => n ** 0.5;
+
+console.log(raizQuadrada(10).toFixed(2));
---
+++ 1javascript-basico/jsbasico-aula31-functions/script.js
@@ -0,0 +1,8 @@
+function hello(nome) {
+    return `Hello ${nome}`;
+}
+
+const variavel = hello('Kaiky');
+const variavel2 = hello('Maria');
+console.log(variavel);
+console.log(variavel2);
